7109a40c3cc6138e9e2c7c52f68ff49b6749012d
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-09 15:54:13 -0400

    Create routes for different pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Location from "./pages/Location";
+import CreateLocation from "./pages/CreateLocation";
+import SearchLocation from "./pages/SearchLocation";
+import Account from "./pages/Account";
 
 function App() {
   return (
-    <>
-      <div>Welcome to my React App.</div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/create" element={<CreateLocation />}></Route>
+        <Route path="/location" element={<Location />}></Route>
+        <Route path="/search" element={<SearchLocation />}></Route>
+        <Route path="/account" element={<Account />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
