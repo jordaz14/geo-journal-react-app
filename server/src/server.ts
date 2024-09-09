a7d6b04da9ac33f6eee59f18bd32b504c31e51e4
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-09 15:51:46 -0400

    Change server console log on start

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,5 +12,5 @@ app.get(`/hello`, (req: Request, res: Response) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
