6ecc47764b8c813ae929051b20531cdc209600cb
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-10 14:13:51 -0400

    Install supabase and nodemon

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,14 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import * as dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+import e from "express";
+
+dotenv.config();
+
+const supabaseUrl: string = process.env.SUPABASE_URL as string;
+const supabaseKey: string = process.env.SUPABASE_KEY as string;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
 
@@ -11,8 +20,25 @@ app.get(`/hello`, (req: Request, res: Response) => {
 });
 
 app.post("/create-location", (req: Request, res: Response) => {
-  console.log("Post Received");
-  res.send({ message: "A new location was created!" });
+  const { locationId } = req.body;
+
+  console.log(locationId);
+
+  async function insertLocationId(id: string) {
+    const { error } = await supabase
+      .from("location_ids")
+      .insert({ location_id: id });
+
+    if (error) {
+      console.error("Error inserting data:", error);
+    } else {
+      console.log("Data Inserted");
+    }
+  }
+
+  insertLocationId(locationId);
+
+  res.send({ message: "A new location was created!", locationId: locationId });
 });
 
 const PORT = process.env.PORT || 5000;
