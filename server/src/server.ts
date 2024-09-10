e73f49933f88ce39014d9c5506c3677ab6380986
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-09 21:12:19 -0400

    Create route handler for creating new locations

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,12 @@ app.use(express.json());
 app.use(cors());
 
 app.get(`/hello`, (req: Request, res: Response) => {
-  res.json({ message: "Hello from the Server!" });
+  res.send({ message: "Hello from the Server!" });
+});
+
+app.post("/create-location", (req: Request, res: Response) => {
+  console.log("Post Received");
+  res.send({ message: "A new location was created!" });
 });
 
 const PORT = process.env.PORT || 5000;
