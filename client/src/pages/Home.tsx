4e034010f98f0505393c89b4419a1b3c03ee4fc9
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-09 21:09:36 -0400

    Add background map image to hero section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,27 +1,31 @@
 import NavBar from "../components/NavBar";
+import MainContainer from "../components/MainContainer";
 import { Link } from "react-router-dom";
 import icon from "../assets/pin.png";
+import bg from "../assets/map.png";
 
 function Home() {
   return (
     <>
       <NavBar></NavBar>
-      <div
-        id="body-container"
-        className="flex-1 flex flex-col justify-center items-center"
-      >
-        <h1 className="text-8xl font-bold">near here</h1>
-        <p className="text-3xl font-karla">see around, be around</p>
-        <div className="flex items-center mt-4">
-          <img src={icon} className="w-10 h-10" alt="Pin Icon" />
+      <MainContainer>
+        <img
+          src={bg}
+          alt="background image "
+          className="absolute opacity-15 w-full h-full"
+        />
+        <h1 className="text-8xl font-bold z-10">near here</h1>
+        <p className="text-3xl font-karla z-10">see around, be around</p>
+        <div className="flex items-center mt-4 z-10">
+          <img src={icon} className="w-10 h-10 z-10" alt="Pin Icon" />
           <Link
             to="/create"
-            className="flex items-center bg-primary-red text-white py-2 px-4 rounded-md shadow-sm"
+            className="flex items-center bg-primary-red text-white py-2 px-4 rounded-md shadow-sm z-10"
           >
             Create Location
           </Link>
         </div>
-      </div>
+      </MainContainer>
     </>
   );
 }
