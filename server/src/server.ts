15b17259b61f006277dfe3fc27bce14b3bcc46f0
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-10 22:05:50 -0400

    Add server side logic to validate if the request location exists

--- server/src/server.ts
+++ server/src/server.ts
@@ -37,6 +37,25 @@ app.post("/create-location", async (req: Request, res: Response) => {
   res.send({ message: "A new location was created!", locationId: locationId });
 });
 
+app.get("/location/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const { data, error } = await supabase
+    .from("location_ids")
+    .select("*")
+    .eq("location_id", id);
+
+  if (error) {
+    return res.send({ Message: "An Error Occurred" });
+  }
+
+  if (data.length == 0) {
+    return res.send({ Message: "Your page does not exist" });
+  }
+
+  res.send({ Message: "Your page does exist!" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
