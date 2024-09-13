578d8f9104e22e5fdce9741fbc37267270983b6f
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-13 09:49:05 -0400

    Create MapComponent which dynamically updates positioning when new coords as passed as props

---
+++ client/src/components/MapComponent.tsx
@@ -0,0 +1,24 @@
+import { TileLayer, useMap, Marker } from "react-leaflet";
+import { useEffect } from "react";
+
+function MapComponent({ coords }: { coords: { lat: number; lng: number } }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (coords.lat && coords.lng) {
+      map.setView([coords.lat, coords.lng], map.getZoom());
+    }
+  }, [coords, map]);
+
+  return (
+    <>
+      <TileLayer
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://carto.com/">Carto</a>'
+      />
+      <Marker position={[coords.lat, coords.lng]} />
+    </>
+  );
+}
+
+export default MapComponent;
