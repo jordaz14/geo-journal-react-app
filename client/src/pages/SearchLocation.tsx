f66124a2016d1475556919343519d3e4426e931f
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-13 09:50:57 -0400

    Stylize & dynamically render location components

--- client/src/pages/SearchLocation.tsx
+++ client/src/pages/SearchLocation.tsx
@@ -1,14 +1,33 @@
 import NavBar from "../components/NavBar";
 import MainContainer from "../components/MainContainer";
 import VerticalContainer from "../components/VerticalContainer";
+import MapComponent from "../components/MapComponent";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import { useState } from "react";
 
 function SearchLocation() {
   const [coords, setCoords] = useState({ lat: 40.6875646, lng: -73.9940103 });
+
   const locations = [
     {
       title: "anybody care to share some thoughts?",
+      coords: { lat: 42.39, lng: -110.59 },
+      updated: "9/14/24",
+      comment: "this is my comment",
+      activity: "12 comments",
+      isOwner: true,
+    },
+    {
+      title: "another random post",
+      coords: { lat: 40.39, lng: -80.82 },
+      updated: "9/14/24",
+      comment: "this is my comment",
+      activity: "12 comments",
+      isOwner: true,
+    },
+    {
+      title: "another random post",
+      coords: { lat: 37.39, lng: -80.82 },
       updated: "9/14/24",
       comment: "this is my comment",
       activity: "12 comments",
@@ -27,38 +46,43 @@ function SearchLocation() {
           <p className="text-xl text-center mt-2">
             see if any others have come along
           </p>
-          <div className="w-full flex-1 p-2 bg-white my-4 flex flex-col justify-center rounded-md drop-shadow-md hover:drop-shadow-2xl transition duration-500 linear relative">
+          <div className="w-full h-[300px] p-2 bg-white my-4 flex flex-col justify-center rounded-md drop-shadow-md hover:drop-shadow-2xl transition duration-500 linear relative">
             <MapContainer
               center={[coords.lat as number, coords.lng as number]}
               zoom={13}
-              className="w-full h-full bg-white "
+              className="w-full h-full bg-white"
             >
-              <TileLayer
-                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-                attribution='&copy; <a href="https://carto.com/">Carto</a>'
-              />
-              <Marker position={[coords.lat as number, coords.lng as number]} />
+              <MapComponent coords={coords}></MapComponent>
             </MapContainer>
           </div>
-          <p className="text-xl text-center">
+          <p className="text-xl text-center mb-4">
             Your location is: {coords.lat} {coords.lng}
           </p>
-          <div className="overflow-auto w-full h-[200px] px-2 mt-4">
-            <div className="bg-white w-full p-4 rounded-md flex flex-col items-center">
-              <h2 className="text-xl text-center font-bold">
-                Anybody care to share some thoughts
-              </h2>
-              <hr />
-              <p>Updated 9/12/24</p>
-              <div>
-                <p>
-                  This is going to be my long comments with a lot of commentary,
-                  and you're going to wonder why this comment is so long.
+          <div className="overflow-y-auto no-scrollbar w-full h-[400px]">
+            {locations.map((location) => (
+              <div
+                onMouseEnter={() =>
+                  setCoords({
+                    lat: location.coords.lat,
+                    lng: location.coords.lng,
+                  })
+                }
+                className="bg-white w-full p-4 rounded-md flex flex-col items-center mb-8 border border-solid border-secondary-gray hover:border-primary-gray transition duration-500 linear"
+              >
+                <h2 className="text-xl text-center font-bold">
+                  {location.title}
+                </h2>
+                <hr className="border-solid border-secondary-gray w-full my-2" />
+                <p>Updated: {location.updated}</p>
+                <div className="bg-red-50 text-center rounded-md my-2 p-2 w-full">
+                  <p>{location.comment}</p>
+                </div>
+                <p>{location.activity}</p>
+                <p className="mt-4">
+                  You're the owner of this location. Click here to delete.
                 </p>
               </div>
-              <p>+12 Comments</p>
-              <p>You're the owner of this location. Click here to delete.</p>
-            </div>
+            ))}
           </div>
         </VerticalContainer>
       </MainContainer>
