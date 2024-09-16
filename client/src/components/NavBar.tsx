d1c7294b26c623e2bfdbf3f763fa8c4dbe9e40e5
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-16 14:43:57 -0400

    Change CTA button to account page if user is logged in

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const isLoginToken: string | null = localStorage.getItem("token");
+
   return (
     <nav className="w-full h-12 bg-white drop-shadow-sm flex align-middle font-lato font-bold text-md">
       <div id="left-nav" className="flex flex-1  px-4">
@@ -12,12 +14,21 @@ function NavBar() {
         <Link to="/search" className="flex items-center">
           locations
         </Link>
-        <Link
-          to="/account"
-          className="flex items-center bg-primary-red text-white py-2 px-4 rounded-md h-[75%]"
-        >
-          log in
-        </Link>
+        {isLoginToken ? (
+          <Link
+            to="/account"
+            className="flex items-center bg-primary-red text-white py-2 px-4 rounded-md h-[75%] "
+          >
+            account
+          </Link>
+        ) : (
+          <Link
+            to="/session"
+            className="flex items-center bg-primary-red text-white py-2 px-4 rounded-md h-[75%]"
+          >
+            log in
+          </Link>
+        )}
       </div>
     </nav>
   );
