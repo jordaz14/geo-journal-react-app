884cf827ca51c0bce0895e2e6c53ff571303a616
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-17 14:45:19 -0400

    Handle auth loading state of app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,34 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Location from "./pages/Location";
+import ViewLocation from "./pages/ViewLocation";
 import CreateLocation from "./pages/CreateLocation";
 import SearchLocation from "./pages/SearchLocation";
 import Session from "./pages/Session";
 import Account from "./pages/Account";
+import { AuthContext } from "./context/AuthContext";
+import { useContext } from "react";
 
 function App() {
+  interface AuthContextType {
+    user: string | null;
+    login: (username: string, password: string) => Promise<void>;
+    logout: () => void;
+    loading: boolean;
+  }
+
+  const { loading } = useContext(AuthContext) as AuthContextType;
+
+  if (loading) return <div></div>;
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/create" element={<CreateLocation />}></Route>
-        <Route path="/location/:id" element={<Location />}></Route>
-        <Route path="/search" element={<SearchLocation />}></Route>
-        <Route path="/session" element={<Session />}></Route>
-        <Route path="/account" element={<Account />}></Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/create" element={<CreateLocation />}></Route>
+      <Route path="/location/:id" element={<ViewLocation />}></Route>
+      <Route path="/search" element={<SearchLocation />}></Route>
+      <Route path="/session" element={<Session />}></Route>
+      <Route path="/account" element={<Account />}></Route>
+    </Routes>
   );
 }
 
