1cee67398297df09d8435c6a9a514cc3e8bdaa61
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-17 14:44:26 -0400

    Wrap App component in Router & AuthProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
 import React, { StrictMode } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import { AuthProvider } from "./context/AuthContext.tsx";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   //<StrictMode>
-    <App />
+  <Router>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Router>
   //</StrictMode>
 );
