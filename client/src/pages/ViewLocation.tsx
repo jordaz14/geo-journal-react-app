065a712dd67656d9865e4b91600cdc8ecdd4acc2
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-09-18 10:06:02 -0400

    Handle entry post & render new component if location exists

--- client/src/pages/ViewLocation.tsx
+++ client/src/pages/ViewLocation.tsx
@@ -9,16 +9,16 @@ import "leaflet/dist/leaflet.css";
 import { serverUrl, postData, fetchData } from "../utils/fetch";
 
 function ViewLocation() {
-  const { id } = useParams();
+  const { locationId } = useParams();
   const [isLoadingLocation, setLoadingLocation] = useState(true);
   const [isLoadingMap, setLoadingMap] = useState(true);
   const [isLocation, setLocation] = useState(false);
+  const [isEntry, setEntry] = useState(false);
   const [coords, setCoords] = useState<{
     lat: null | number;
     lng: null | number;
   }>({ lat: null, lng: null });
   const [formData, setFormData] = useState({ message: "" });
-  const isLoginToken: string | null = localStorage.getItem("token");
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -29,28 +29,30 @@ function ViewLocation() {
   function formSubmit(e) {
     e.preventDefault();
 
-    postData(
-      `${serverUrl}/create-entry`,
-      {
-        data: [coords, formData],
-      },
-      {
-        Authorization: `Bearer ${isLoginToken}`,
-        "Content-Type": "application/json",
-      }
-    ).then((response) => console.log(response));
+    postData(`${serverUrl}/entry/${locationId}`, {
+      coords,
+      formData,
+    }).then((response) => {
+      console.log("this is executing");
+      console.log(response);
+      setEntry(true);
+    });
   }
 
   useEffect(() => {
-    fetchData(`${serverUrl}/location/${id}`).then((response) => {
+    fetchData(`${serverUrl}/location/${locationId}`).then((response) => {
       console.log(response);
       setLoadingLocation(false);
       if (response.isLocation) {
         setLocation(true);
-        getCoords();
+        if (response.isEntry) {
+          setEntry(true);
+        } else {
+          getCoords();
+        }
       }
     });
-  }, [id]);
+  }, [locationId]);
 
   function getCoords() {
     if ("geolocation" in navigator) {
@@ -76,51 +78,57 @@ function ViewLocation() {
         {!isLoadingLocation ? (
           isLocation ? (
             <VerticalContainer>
-              <div
-                id="feed"
-                className=" w-full flex-1 flex flex-col mb-4 items-center justify-center"
-              >
-                <h1 className="text-4xl font-bold text-center">
-                  it's a big world out there
-                </h1>
-                <p className="text-xl text-center mt-2">
-                  but you have something to share, <br></br> so what do you say?
-                </p>
-                {!isLoadingMap ? (
-                  <>
-                    <div className="w-full flex-1 p-2 bg-white my-4 flex flex-col justify-center rounded-md drop-shadow-md hover:drop-shadow-2xl transition duration-500 linear relative">
-                      <MapContainer
-                        center={[coords.lat as number, coords.lng as number]}
-                        zoom={13}
-                        className="w-full h-full bg-white "
-                      >
-                        <TileLayer
-                          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-                          attribution='&copy; <a href="https://carto.com/">Carto</a>'
-                        />
-                        <Marker
-                          position={[
-                            coords.lat as number,
-                            coords.lng as number,
-                          ]}
-                        />
-                      </MapContainer>
-                    </div>
-                    <p className="text-xl text-center">
-                      Your location is: {coords.lat} {coords.lng}
-                    </p>
-                  </>
-                ) : (
-                  <>
-                    <div className="w-full flex-1 p-2 bg-white my-4 flex flex-col justify-center items-center rounded-md drop-shadow-md hover:drop-shadow-2xl transition duration-500 linear relative">
-                      <div id="loader"></div>
-                    </div>
-                    <p className="text-xl text-center">
-                      Your location is loading. <br></br> &nbsp;
-                    </p>
-                  </>
-                )}
-              </div>
+              {isEntry ? (
+                <h1>My entries</h1>
+              ) : (
+                <div
+                  id="feed"
+                  className=" w-full flex-1 flex flex-col mb-4 items-center justify-center"
+                >
+                  <h1 className="text-4xl font-bold text-center">
+                    it's a big world out there
+                  </h1>
+                  <p className="text-xl text-center mt-2">
+                    but you have something to share, <br></br> so what do you
+                    say?
+                  </p>
+                  {!isLoadingMap ? (
+                    <>
+                      <div className="w-full flex-1 p-2 bg-white my-4 flex flex-col justify-center rounded-md drop-shadow-md hover:drop-shadow-2xl transition duration-500 linear relative">
+                        <MapContainer
+                          center={[coords.lat as number, coords.lng as number]}
+                          zoom={13}
+                          className="w-full h-full bg-white "
+                        >
+                          <TileLayer
+                            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                            attribution='&copy; <a href="https://carto.com/">Carto</a>'
+                          />
+                          <Marker
+                            position={[
+                              coords.lat as number,
+                              coords.lng as number,
+                            ]}
+                          />
+                        </MapContainer>
+                      </div>
+                      <p className="text-xl text-center">
+                        Your location is: {coords.lat} {coords.lng}
+                      </p>
+                    </>
+                  ) : (
+                    <>
+                      <div className="w-full flex-1 p-2 bg-white my-4 flex flex-col justify-center items-center rounded-md drop-shadow-md hover:drop-shadow-2xl transition duration-500 linear relative">
+                        <div id="loader"></div>
+                      </div>
+                      <p className="text-xl text-center">
+                        Your location is loading. <br></br> &nbsp;
+                      </p>
+                    </>
+                  )}
+                </div>
+              )}
+
               <form id="input" className="w-full" onSubmit={formSubmit}>
                 <textarea
                   id="message"
