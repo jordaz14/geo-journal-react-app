cb0f6bb2db65157c576160e1519f1baf8fba710e
Julian <1julianordaz@gmail.com>
2024-09-24 23:00:57 -0600

    Add responsize width to vertical container

--- client/src/components/VerticalContainer.tsx
+++ client/src/components/VerticalContainer.tsx
@@ -6,7 +6,7 @@ interface VerticalContainerProps {
 
 const VerticalContainer = ({ children }: VerticalContainerProps) => {
   return (
-    <div className="w-[500px] h-full flex flex-col items-center py-2 px-2">
+    <div className="w-full sm:w-[500px] h-full flex flex-col items-center py-2 px-2">
       {children}
     </div>
   );
