46bb7f0ac4f5d9aaff01bf07acc035f7df196158
Julian <1julianordaz@gmail.com>
2024-09-25 20:20:12 -0600

    Update client url

--- client/src/utils/fetch.ts
+++ client/src/utils/fetch.ts
@@ -1,5 +1,5 @@
 export const clientUrl = import.meta.env.PROD
-  ? "https://nearhere.netlify.app/"
+  ? "https://nearhere.netlify.app"
   : "http://localhost:5173";
 
 export const serverUrl = import.meta.env.PROD
