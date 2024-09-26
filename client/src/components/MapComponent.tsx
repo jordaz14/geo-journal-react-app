bb2ca8d1b05b9738e12d4b43bb9b0856d6ac2e99
Julian <1julianordaz@gmail.com>
2024-09-25 23:34:00 -0600

    Import leaflet icons to display on deployment

--- client/src/components/MapComponent.tsx
+++ client/src/components/MapComponent.tsx
@@ -1,5 +1,9 @@
 import { TileLayer, useMap, Marker } from "react-leaflet";
+import { Icon } from "leaflet";
 import { useEffect } from "react";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIconRetina from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 const MapComponent = ({ coords }: { coords: { lat: number; lng: number } }) => {
   const map = useMap();
@@ -16,7 +20,20 @@ const MapComponent = ({ coords }: { coords: { lat: number; lng: number } }) => {
         url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://carto.com/">Carto</a>'
       />
-      <Marker position={[coords.lat, coords.lng]} />
+      <Marker
+        position={[coords.lat, coords.lng]}
+        icon={
+          new Icon({
+            iconUrl: markerIcon,
+            iconRetinaUrl: markerIconRetina,
+            shadowUrl: markerShadow,
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41],
+          })
+        }
+      />
     </>
   );
 };
